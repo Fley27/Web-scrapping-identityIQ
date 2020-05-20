3ccaba9f7fb880e1d448a56d6bdbb18ded5c2b57
Fley027 <fleyvick@outlook.com>
2020-05-20 11:57:50 -0400

    Scores

--- scrapper.js
+++ scrapper.js
@@ -5,15 +5,13 @@ const PASSWORD_SELECTOR = "#txtPassword";
 const CTA_SELECTOR = "#imgBtnLogin";
 const CODE_SELECTOR = "#FBfbforcechangesecurityanswer_txtSecurityAnswer";
 const SUBMIT_SELECTOR = "#FBfbforcechangesecurityanswer_ibtSubmit";
-const NEXTDATE = "#Fbscore_lblNextRefDate";
 
-const webScraping = async (scraper) => {
+const scrapper = async ({ username, password, last4 }) => {
   // Create an instance of the chrome browser
   // But disable headless mode !
-  let name = null;
   let dbObj = {};
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
   });
 
   // Create a new page
@@ -38,16 +36,42 @@ const webScraping = async (scraper) => {
     await page.keyboard.type(C.code);
     await page.click(SUBMIT_SELECTOR);
 
-    await page.goto("https://www.identityiq.com/Dashboard.aspx", {
+    await page.goto("https://www.identityiq.com/CreditReport.aspx", {
       waitUntil: "load",
       timeout: 0,
     });
-    const [elementHandle] = await page.$x(
-      "/html/body/form/div[6]/div[2]/div[3]/div/div[1]/div[1]/div[5]/span"
+    // Bureau  Experian
+    await page.waitForXPath(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[3]`
     );
-    const propertyHandle = await elementHandle.getProperty("textContent");
+    const [experian] = await page.$x(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[3]`
+    );
+    const expeHandle = await experian.getProperty(`textContent`);
+    const expeValue = await expeHandle.jsonValue();
+    //TransUnion
+    await page.waitForXPath(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[2]`
+    );
+    const [elementHandleUnion] = await page.$x(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[2]`
+    );
+    const propertyHandle = await elementHandleUnion.getProperty(`textContent`);
     const propertyValue = await propertyHandle.jsonValue();
-    console.log(`REFRESH DATE (Next available) : ${propertyValue}`);
+    //Bureau EquiFax
+    await page.waitForXPath(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[4]`
+    );
+    const [equifax] = await page.$x(
+      `/html/body/form/div[6]/div/div[3]/div[2]/div/div/div[5]/div/transunion-report/div[2]/div[5]/table[2]/tbody/tr[2]/td[4]`
+    );
+    const equifaxHandle = await equifax.getProperty(`textContent`);
+    const equifaxValue = await equifaxHandle.jsonValue();
+    //Print everything
+
+    console.log(
+      `TransUnion : ${propertyValue} ---- Experian : ${expeValue} ---- Equifax : ${equifaxValue} `
+    );
   } catch (error) {
     console.log(`${error}`);
   }
@@ -55,10 +79,4 @@ const webScraping = async (scraper) => {
   return dbObj;
 };
 
-webScraping();
-/*const newDom = await page.evaluate => 
-      const el = document.getElementById"#Fbscore_lblNextRefDate".innerText;
-      return el;
-    ;
-
-    dbObj.item = newDom;*/
+scrapper("", "", "");
